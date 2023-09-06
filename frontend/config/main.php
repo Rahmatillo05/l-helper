f69c5a9ba5ac89233ba8ff23a82c9244356571dc
Rahmatullo Husanboyev <hrrahmatillo2oo5@gmail.com>
2023-09-06 07:38:56 +0500

    initial commit

---
+++ frontend/config/main.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\rest\UrlRule;
+
+$params = array_merge(
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-frontend',
+            'baseUrl' => '',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ]
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced-frontend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => \yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'scriptUrl' => '/index.php',
+            'rules' => [
+                [
+                    'class' => UrlRule::class,
+                    'controller' => ['site']
+                ]
+            ],
+        ],
+
+    ],
+    'params' => $params,
+];
---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\rest\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex(): string
+    {
+        return "Say hello";
+    }
+}
